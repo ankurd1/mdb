8353c12828ceffc6e6ad24899ca4240d327ba0e5
Ankur Dahiya <legalos.lotr@gmail.com>
2012-06-29 10:05:32 +0530

    Improvements to linux setup

--- setup.py
+++ setup.py
@@ -44,16 +44,24 @@ lin_nautilus_script_in = 'setup/linux/nautilus-script.py'
 
 if __name__ == '__main__':
     platform = get_platform()
-    if (platform == 'windows'):
+    action = sys.argv[1]
+
+    if (platform == 'windows' and action == 'py2exe'):
         setup_options.update(win_options)
         setup(**setup_options)
         # TODO create the installer
     elif (platform == 'linux'):
+        # TODO Also install a .desktop file and an uninstaler.
         setup_options.update(lin_options)
         setup(**setup_options)
 
-        # copy nautilus-shortcuts.py to nautilus scripts
-        shutil.copy(lin_nautilus_script_in, lin_nautilus_script_out)
-        os.chmod(lin_nautilus_script_out, 0777)
+        if (action == 'install'):
+            print "Do you want to install right-click shortcuts for nautilus/gnome?"
+            ans = sys.stdin.readline()
+            if ans.lower().startswith('y'):
+                # copy nautilus-shortcuts.py to nautilus scripts
+                shutil.copy(lin_nautilus_script_in, lin_nautilus_script_out)
+                os.chmod(lin_nautilus_script_out, 0777)
+                print "Shortcuts installed successfully."
     else:
-        print "Platform not supported."
+        print "Platform/action not supported."
